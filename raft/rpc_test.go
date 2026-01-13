dfd58288b74259220e2d622508932fdadd0d0e9d
Mike Rhodes <mike.rhodes@gmail.com>
2026-01-13 20:31:37 +0000

    test: add test that a new leader is elected after one is killed
    This isn't the cleanest test; I will let it stew and clean it up later.
    For now, the test shows that the cluster will regain a leader if the
    leader is killed. If the old leader later comes back online, it happily
    remains a follower. This is good.

--- raft/rpc_test.go
+++ raft/rpc_test.go
@@ -114,35 +114,180 @@ func TestThreeServersOneLeaderAtStartup(t *testing.T) {
 		}
 	}
 
-	// Wait for election timeout (150-300ms) plus some buffer
-	time.Sleep(350 * time.Millisecond)
-
 	// Two servers should be followers, one should be leader
-	leaders, followers, candidates := 0, 0, 0
-	for _, srv := range servers {
-		switch srv.Role() {
-		case RaftRoleFollower:
-			followers += 1
-		case RaftRoleCandidate:
-			candidates += 1
-		case RaftRoleLeader:
-			leaders += 1
+	wantedState := makeWantedState(t, 1, 0, 2)
+	waitForWantedState(t, servers, wantedState)
+
+	// Clean up
+	for id, srv := range servers {
+		srv.Shutdown()
+		raftGroup[id].listener.Close()
+	}
+}
+
+func TestThreeServersOneLeaderAfterLeaderDies(t *testing.T) {
+	// Generate the "template" for each raft server.
+	type Peer struct {
+		id       ServerId
+		listener net.Listener
+		addr     net.Addr
+	}
+	raftGroup := []Peer{}
+	for id := range 3 {
+		l, err := net.Listen("tcp", ":0")
+		if err != nil {
+			t.Fatalf("Couldn't open tcp listener: %v", err)
+		}
+		raftGroup = append(raftGroup, Peer{
+			id:       ServerId(id),
+			listener: l,
+			addr:     l.Addr(),
+		})
+	}
+
+	servers := []*RaftServer{}
+	for _, peer := range raftGroup {
+		peerAddrs := []net.Addr{}
+		for _, cp := range raftGroup {
+			if cp.id != peer.id {
+				peerAddrs = append(peerAddrs, cp.addr)
+			}
+		}
+
+		srv, err := NewRaftServer(peer.id, peerAddrs)
+		if err != nil {
+			t.Fatalf("NewRaftServer failed: %v", err)
+		}
+		go srv.Start(peer.listener)
+
+		servers = append(servers, srv)
+	}
+
+	// All servers should start as Followers
+	for id, srv := range servers {
+		if role := srv.Role(); role != RaftRoleFollower {
+			t.Errorf("srv[%d].Role() = %s, want RaftRoleFollower", id, role)
 		}
 	}
 
-	if leaders != 1 {
-		t.Errorf("leaders = %d, want 1", leaders)
+	// Two servers should be followers, one should be leader
+	wantedState := makeWantedState(t, 1, 0, 2)
+	waitForWantedState(t, servers, wantedState)
+	var firstLeaderId ServerId
+	for _, srv := range servers {
+		if srv.Role() == RaftRoleLeader {
+			firstLeaderId = srv.serverId
+		}
 	}
-	if followers != 2 {
-		t.Errorf("followers = %d, want 2", followers)
+
+	// Kill current leader
+	servers[firstLeaderId].Shutdown()
+	servers[firstLeaderId] = nil
+
+	t.Logf("KILLED THE LEADER %d", firstLeaderId)
+
+	// Now we should have 1 leader and 1 follower
+	wantedState = makeWantedState(t, 1, 0, 1)
+	waitForWantedState(t, servers, wantedState)
+	var secondLeaderId ServerId
+	for _, srv := range servers {
+		if srv == nil {
+			continue // removed the leader
+		}
+		if srv.Role() == RaftRoleLeader {
+			secondLeaderId = srv.serverId
+		}
 	}
-	if candidates != 0 {
-		t.Errorf("candidates = %d, want 0", candidates)
+
+	// Create a new instance for server with ID firstLeaderId
+	// This is like firstLeaderId came back online after a restart.
+	peerAddrs := []net.Addr{}
+	for _, cp := range raftGroup {
+		if cp.id != firstLeaderId {
+			peerAddrs = append(peerAddrs, cp.addr)
+		}
+	}
+	var err error
+	servers[firstLeaderId], err = NewRaftServer(firstLeaderId, peerAddrs)
+	if err != nil {
+		t.Fatalf("Error creating new server for leaderID %d", firstLeaderId)
+	}
+	go servers[firstLeaderId].Start(raftGroup[firstLeaderId].listener)
+	t.Logf("REPLACED SERVER AT %d", firstLeaderId)
+
+	// Should be back to one leader with two followers, and the
+	// leader should not have changed.
+	wantedState = makeWantedState(t, 1, 0, 2)
+	waitForWantedState(t, servers, wantedState)
+	var thirdLeaderId ServerId
+	for _, srv := range servers {
+		if srv.Role() == RaftRoleLeader {
+			thirdLeaderId = srv.serverId
+		}
+	}
+	if thirdLeaderId != secondLeaderId {
+		t.Errorf("thirdLeaderId = %d, want %d", thirdLeaderId, secondLeaderId)
 	}
 
 	// Clean up
 	for id, srv := range servers {
 		srv.Shutdown()
 		raftGroup[id].listener.Close()
+	}
+}
+
+// makeWantedState creates a checker that a state is what
+// is wanted.
+func makeWantedState(t *testing.T, l, c, f int) func(l, c, f int, lastIteration bool) bool {
+	return func(gotL, gotC, gotF int, lastIteration bool) bool {
+		result := true
+		if gotL != l {
+			if lastIteration {
+				t.Errorf("leaders = %d, want 1", gotL)
+			}
+			result = false
+		}
+		if gotC != c {
+			if lastIteration {
+				t.Errorf("candidates = %d, want 0", gotC)
+			}
+			result = false
+		}
+		if gotF != f {
+			if lastIteration {
+				t.Errorf("followers = %d, want 2", gotF)
+			}
+			result = false
+		}
+		return result
+	}
+}
+
+// waitForWantedState waits a bounded amount of time for a given
+// state to be true.
+func waitForWantedState(t *testing.T, servers []*RaftServer, wantedState func(l int, c int, f int, lastIteration bool) bool) {
+	leaders, followers, candidates := 0, 0, 0
+	for range 100 {
+		leaders, followers, candidates = 0, 0, 0
+		for _, srv := range servers {
+			if srv == nil {
+				continue // removed the leader
+			}
+			switch srv.Role() {
+			case RaftRoleFollower:
+				followers += 1
+			case RaftRoleCandidate:
+				candidates += 1
+			case RaftRoleLeader:
+				leaders += 1
+			}
+		}
+		if wantedState(leaders, candidates, followers, false) {
+			break
+		}
+		time.Sleep(25 * time.Millisecond)
+	}
+	if !wantedState(leaders, candidates, followers, true) {
+		t.Fatal("Never reached required state")
 	}
 }
