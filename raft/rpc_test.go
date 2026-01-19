139b6403ff8fb12dcd4dcfe69d4a22cd8bc9d0c4
Mike Rhodes <mike.rhodes@gmail.com>
2026-01-19 21:03:07 +0000

    test: Add test harness for clearer testing
    The harness abstracts the details of creating clusters, shutting down
    clusters, and manipulating them. Tests are now much clearer as they
    can be written in terms of cluster operations rather than the details.

--- raft/rpc_test.go
+++ raft/rpc_test.go
@@ -1,13 +1,13 @@
 package raft
 
 import (
-	"fmt"
-	"net"
 	"testing"
 	"time"
 )
 
 func TestSingleServerChangesTermAppendEntries(t *testing.T) {
+	// Here we don't start a server, but instead test that it
+	// responds to an AE request correctly.
 	stateDir := t.TempDir()
 	raftSrv, err := NewRaftServer(1, []ServerId{}, stateDir)
 	if err != nil {
@@ -45,136 +45,46 @@ func TestSingleServerChangesTermAppendEntries(t *testing.T) {
 }
 
 func TestSingleServerBecomesLeader(t *testing.T) {
-	stateDir := t.TempDir()
-	transport, err := NewHttpTransport()
-	if err != nil {
-		t.Fatalf("NewHttpTransport failed: %v", err)
-	}
-	raftSrv, err := NewRaftServer(1, []ServerId{}, stateDir)
-	if err != nil {
-		t.Fatalf("NewRaftServer failed: %v", err)
-	}
-	transport.Host(raftSrv)
-
-	go transport.Serve()
-	go raftSrv.Start()
-
-	// Server should start as follower
-	if role := raftSrv.Role(); role != RaftRoleFollower {
-		t.Errorf("srv.Role() = %s, want RaftRoleFollower", role)
-	}
+	harness := NewHarness(t, 1)
+	harness.StartAll()
 
 	// Wait for election timeout (150-300ms) plus some buffer
 	time.Sleep(350 * time.Millisecond)
 
 	// Server should be the leader, because there is only one
 	// server, so by voting for itself it is in the majority.
-	if role := raftSrv.Role(); role != RaftRoleLeader {
-		t.Errorf("srv.Role() = %s, want RaftRoleLeader", role)
+	for raft := range harness.Servers() {
+		role := raft.Role()
+		// "all" of the one servers should be leader
+		if role != RaftRoleLeader {
+			t.Errorf("[%d] srv.Role() = %s, want RaftRoleLeader",
+				raft.serverId, role)
+		}
 	}
 
-	// Clean up
-	raftSrv.Shutdown()
-	transport.Shutdown()
+	harness.ShutdownAll()
 }
 
 func TestThreeServersOneLeaderAtStartup(t *testing.T) {
-	transports := map[ServerId]*HttpTransport{}
-	raftServers := map[ServerId]*RaftServer{}
-	stateDirs := map[ServerId]string{}
-	addrs := map[ServerId]net.Addr{}
-	serverIds := []ServerId{1, 2, 3}
-	for _, id := range serverIds {
-		transport, err := NewHttpTransport()
-		if err != nil {
-			t.Fatalf("NewHttpTransport failed: %v", err)
-		}
-		stateDir := t.TempDir()
-		raftSrv, err := NewRaftServer(id, filter(serverIds, id), stateDir)
-		if err != nil {
-			t.Fatalf("NewRaftServer failed: %v", err)
-		}
-		transport.Host(raftSrv)
+	harness := NewHarness(t, 3)
+	harness.StartAll()
 
-		transports[id] = transport
-		raftServers[id] = raftSrv
-		stateDirs[id] = stateDir
-		addrs[id] = transport.Addr()
-	}
-	// Tell each transport about the others' addresses
-	for _, transport := range transports {
-		transport.peerAddrs = addrs
-	}
-	// Start everything up
-	for _, transport := range transports {
-		go transport.Serve()
-	}
-	for _, raft := range raftServers {
-		go raft.Start()
-	}
+	// A leader should emerge
+	harness.WaitForOneLeader(t)
 
-	// All servers should start as Followers
-	for id, raft := range raftServers {
-		if role := raft.Role(); role != RaftRoleFollower {
-			t.Errorf("srv[%d].Role() = %s, want RaftRoleFollower", id, role)
-		}
-	}
-
-	// Two servers should be followers, one should be leader
-	wantedState := makeWantedState(1, 0, 2)
-	waitForWantedState(t, raftServers, wantedState)
-
-	// Clean up
-	for _, transport := range transports {
-		transport.Shutdown()
-	}
-	for _, raft := range raftServers {
-		raft.Shutdown()
-	}
+	harness.ShutdownAll()
 }
 
 func TestThreeServersPersistTerm(t *testing.T) {
-	transports := map[ServerId]*HttpTransport{}
-	raftServers := map[ServerId]*RaftServer{}
-	stateDirs := map[ServerId]string{}
-	addrs := map[ServerId]net.Addr{}
-	serverIds := []ServerId{1, 2, 3}
-	for _, id := range serverIds {
-		transport, err := NewHttpTransport()
-		if err != nil {
-			t.Fatalf("NewHttpTransport failed: %v", err)
-		}
-		stateDir := t.TempDir()
-		raftSrv, err := NewRaftServer(id, filter(serverIds, id), stateDir)
-		if err != nil {
-			t.Fatalf("NewRaftServer failed: %v", err)
-		}
-		transport.Host(raftSrv)
-
-		transports[id] = transport
-		raftServers[id] = raftSrv
-		stateDirs[id] = stateDir
-		addrs[id] = transport.Addr()
-	}
-	// Tell each transport about the others' addresses
-	for _, transport := range transports {
-		transport.peerAddrs = addrs
-	}
-	// Start everything up
-	for _, transport := range transports {
-		go transport.Serve()
-	}
-	for _, raft := range raftServers {
-		go raft.Start()
-	}
+	harness := NewHarness(t, 3)
+	harness.StartAll()
 
 	// Wait for leader
-	wantedState := makeWantedState(1, 0, 2)
-	waitForWantedState(t, raftServers, wantedState)
+	harness.WaitForOneLeader(t)
 
 	// Get the leader's term before shutdown
 	var leaderTerm Term
-	for _, raft := range raftServers {
+	for raft := range harness.Servers() {
 		if raft.Role() == RaftRoleLeader {
 			leaderTerm = raft.CurrentTerm()
 			break
@@ -182,20 +92,13 @@ func TestThreeServersPersistTerm(t *testing.T) {
 	}
 
 	// Shutdown all servers to ensure state is persisted
-	for _, raft := range raftServers {
-		raft.Shutdown()
-	}
-	for _, transport := range transports {
-		transport.Shutdown()
-	}
+	harness.ShutdownAll()
 
 	// Check state for each server
+	// They should all be on the leader term and at least
+	// two should have voted during the term.
 	nVoted := 0
-	for id, stateDir := range stateDirs {
-		ps, err := LoadPersistentState(stateDir)
-		if err != nil {
-			t.Fatalf("Failed to load persistent state for server %d: %v", id, err)
-		}
+	for id, ps := range harness.PersistentStates(t) {
 		if ps.CurrentTerm != leaderTerm {
 			t.Errorf("Server %d ps.CurrentTerm = %d, want %d", id, ps.CurrentTerm, leaderTerm)
 		}
@@ -211,99 +114,48 @@ func TestThreeServersPersistTerm(t *testing.T) {
 }
 
 func TestThreeServersOneLeaderAfterLeaderDies(t *testing.T) {
-	transports := map[ServerId]*HttpTransport{}
-	raftServers := map[ServerId]*RaftServer{}
-	stateDirs := map[ServerId]string{}
-	addrs := map[ServerId]net.Addr{}
-	serverIds := []ServerId{1, 2, 3}
-	for _, id := range serverIds {
-		transport, err := NewHttpTransport()
-		if err != nil {
-			t.Fatalf("NewHttpTransport failed: %v", err)
-		}
-		stateDir := t.TempDir()
-		raftSrv, err := NewRaftServer(id, filter(serverIds, id), stateDir)
-		if err != nil {
-			t.Fatalf("NewRaftServer failed: %v", err)
-		}
-		transport.Host(raftSrv)
-
-		transports[id] = transport
-		raftServers[id] = raftSrv
-		stateDirs[id] = stateDir
-		addrs[id] = transport.Addr()
-	}
-	// Tell all transports about all the others' addresses
-	for _, transport := range transports {
-		transport.peerAddrs = addrs
-	}
-	// Start everything
-	for _, transport := range transports {
-		go transport.Serve()
-	}
-	for _, raft := range raftServers {
-		go raft.Start()
-	}
-
-	// All servers should start as Followers
-	for id, raft := range raftServers {
-		if role := raft.Role(); role != RaftRoleFollower {
-			t.Errorf("srv[%d].Role() = %s, want RaftRoleFollower", id, role)
-		}
-	}
+	harness := NewHarness(t, 3)
+	harness.StartAll()
 
 	// Two servers should be followers, one should be leader
 	// Save the leader ID as we will kill it.
-	wantedState := makeWantedState(1, 0, 2)
-	waitForWantedState(t, raftServers, wantedState)
+	harness.WaitForOneLeader(t)
 	var firstLeaderId ServerId
-	for _, srv := range raftServers {
-		if srv.Role() == RaftRoleLeader {
-			firstLeaderId = srv.serverId
+	for raft := range harness.Servers() {
+		if raft.Role() == RaftRoleLeader {
+			firstLeaderId = raft.serverId
+			break
 		}
 	}
 
-	// Kill current leader --- leave the transport for
-	// reuse later as we can't reconfigure a cluster.
-	raftServers[firstLeaderId].Shutdown()
-	transports[firstLeaderId].Shutdown()
-	delete(raftServers, firstLeaderId)
+	// Kill the current leader
+	harness.Shutdown(firstLeaderId)
 	t.Logf("KILLED THE LEADER %d", firstLeaderId)
 
-	// Now we should have 1 leader and 1 follower
-	wantedState = makeWantedState(1, 0, 1)
-	waitForWantedState(t, raftServers, wantedState)
+	// Wait for a leader to emerge
+	harness.WaitForOneLeader(t)
 	var secondLeaderId ServerId
-	for _, srv := range raftServers {
-		if srv == nil {
-			continue // removed the leader
-		}
-		if srv.Role() == RaftRoleLeader {
-			secondLeaderId = srv.serverId
+	for raft := range harness.Servers() {
+		if raft.Role() == RaftRoleLeader {
+			secondLeaderId = raft.serverId
+			break
 		}
 	}
 
-	// Create a new instance for server with ID firstLeaderId
-	// This is like firstLeaderId came back online after a restart.
-	var err error
-	newRaftServer, err := NewRaftServer(firstLeaderId, filter(serverIds, firstLeaderId), stateDirs[firstLeaderId])
-	newRaftServer.transport = transports[firstLeaderId] // reuse transport
+	// Restart the first leader -- it should end up a follower
+	err := harness.Restart(firstLeaderId)
 	if err != nil {
-		t.Fatalf("Error creating new server for leaderID %d", firstLeaderId)
+		t.Fatalf("Error restarting server: %v", err)
 	}
-	raftServers[firstLeaderId] = newRaftServer
-	go transports[firstLeaderId].Serve()
-	go newRaftServer.Start()
-	t.Logf("REPLACED SERVER AT %d", firstLeaderId)
+	t.Logf("RESTARTED SERVER AT %d", firstLeaderId)
 
-	// Should be back to one leader with two followers, and the
-	// leader should not have changed.
-	wantedState = makeWantedState(1, 0, 2)
-	waitForWantedState(t, raftServers, wantedState)
+	// Leader should not change, we shouldn't have an election
+	harness.WaitForOneLeader(t)
 	var thirdLeaderId ServerId
-	for _, srv := range raftServers {
-		if srv.Role() == RaftRoleLeader {
-			thirdLeaderId = srv.serverId
+	for raft := range harness.Servers() {
+		if raft.Role() == RaftRoleLeader {
+			thirdLeaderId = raft.serverId
+			break
 		}
 	}
 	if thirdLeaderId != secondLeaderId {
@@ -311,77 +163,5 @@ func TestThreeServersOneLeaderAfterLeaderDies(t *testing.T) {
 	}
 
 	// Clean up
-	for _, transport := range transports {
-		transport.Shutdown()
-	}
-	for _, raft := range raftServers {
-		raft.Shutdown()
-	}
-}
-
-// filter removes items matching `remove` from `list`.
-// Used to create the list of peers for a given raft server such
-// that the list doesn't include the server itself.
-func filter[T comparable](list []T, remove T) []T {
-	filtered := []T{}
-	for _, item := range list {
-		if item != remove {
-			filtered = append(filtered, item)
-		}
-	}
-	return filtered
-}
-
-// makeWantedState creates a checker that a state is what
-// is wanted.
-func makeWantedState(wantL, wantC, wantF int) func(l, c, f int, lastIteration bool) (bool, []error) {
-	return func(gotL, gotC, gotF int, lastIteration bool) (bool, []error) {
-		result := true
-		errs := []error{}
-		if gotL != wantL {
-			errs = append(errs, fmt.Errorf("leaders = %d, want %d", gotL, wantL))
-			result = false
-		}
-		if gotC != wantC {
-			errs = append(errs, fmt.Errorf("candidates = %d, want %d", gotC, wantC))
-			result = false
-		}
-		if gotF != wantF {
-			errs = append(errs, fmt.Errorf("followers = %d, want %d", gotF, wantF))
-			result = false
-		}
-		return result, errs
-	}
-}
-
-// waitForWantedState waits a bounded amount of time for a given
-// state to be true.
-func waitForWantedState(t *testing.T, servers map[ServerId]*RaftServer, wantedState func(l int, c int, f int, lastIteration bool) (bool, []error)) {
-	leaders, followers, candidates := 0, 0, 0
-	for range 100 {
-		leaders, followers, candidates = 0, 0, 0
-		for _, srv := range servers {
-			if srv == nil {
-				continue // removed the leader
-			}
-			switch srv.Role() {
-			case RaftRoleFollower:
-				followers += 1
-			case RaftRoleCandidate:
-				candidates += 1
-			case RaftRoleLeader:
-				leaders += 1
-			}
-		}
-		if ok, _ := wantedState(leaders, candidates, followers, false); ok {
-			break
-		}
-		time.Sleep(25 * time.Millisecond)
-	}
-	if ok, errs := wantedState(leaders, candidates, followers, false); !ok {
-		for _, err := range errs {
-			t.Error(err)
-		}
-		t.Fatal("Never reached required state")
-	}
+	harness.ShutdownAll()
 }
---
+++ raft/testharness.go
@@ -0,0 +1,172 @@
+package raft
+
+import (
+	"fmt"
+	"iter"
+	"net"
+	"testing"
+	"time"
+)
+
+type Harness struct {
+	transports  map[ServerId]*HttpTransport
+	raftServers map[ServerId]*RaftServer
+	stateDirs   map[ServerId]string
+	addrs       map[ServerId]net.Addr
+	serverIds   []ServerId
+}
+
+// NewHarness creates a number of raft servers with HTTP transports,
+// starting at server ID 1.
+func NewHarness(t *testing.T, n int) *Harness {
+	t.Helper()
+	serverIds := []ServerId{}
+	for id := range n {
+		serverIds = append(serverIds, ServerId(id+1)) // start IDs at 1
+	}
+	transports := map[ServerId]*HttpTransport{}
+	raftServers := map[ServerId]*RaftServer{}
+	stateDirs := map[ServerId]string{}
+	addrs := map[ServerId]net.Addr{}
+	for _, id := range serverIds {
+		transport, err := NewHttpTransport()
+		if err != nil {
+			t.Fatalf("NewHttpTransport failed: %v", err)
+		}
+		stateDir := t.TempDir()
+		raftSrv, err := NewRaftServer(id, filter(serverIds, id), stateDir)
+		if err != nil {
+			t.Fatalf("NewRaftServer failed: %v", err)
+		}
+		transport.Host(raftSrv)
+
+		transports[id] = transport
+		raftServers[id] = raftSrv
+		stateDirs[id] = stateDir
+		addrs[id] = transport.Addr()
+	}
+	// Tell each transport about the others' addresses
+	for _, transport := range transports {
+		transport.peerAddrs = addrs
+	}
+	return &Harness{
+		transports:  transports,
+		raftServers: raftServers,
+		stateDirs:   stateDirs,
+		addrs:       addrs,
+		serverIds:   serverIds,
+	}
+}
+
+func (h *Harness) StartAll() {
+	// Start everything up
+	for _, transport := range h.transports {
+		go transport.Serve()
+	}
+	for _, raft := range h.raftServers {
+		go raft.Start()
+	}
+}
+
+func (h *Harness) ShutdownAll() {
+	for _, transport := range h.transports {
+		transport.Shutdown()
+	}
+	for _, raft := range h.raftServers {
+		raft.Shutdown()
+	}
+}
+
+// WaitForOneLeader waits for there to be a stable cluster,
+// 1 leader and 0 candidates.
+func (h *Harness) WaitForOneLeader(t *testing.T) {
+	t.Helper()
+	leaders, followers, candidates := 0, 0, 0
+	for range 100 {
+		leaders, followers, candidates = 0, 0, 0
+		for _, srv := range h.raftServers {
+			if srv == nil {
+				continue // removed the leader
+			}
+			switch srv.Role() {
+			case RaftRoleFollower:
+				followers += 1
+			case RaftRoleCandidate:
+				candidates += 1
+			case RaftRoleLeader:
+				leaders += 1
+			}
+		}
+		if leaders == 1 && candidates == 0 {
+			break
+		}
+		time.Sleep(5 * time.Millisecond)
+	}
+	if !(leaders == 1 && candidates == 0) {
+		t.Fatalf("got leaders = %d, candidates = %d, wanted 1, 0",
+			leaders, candidates)
+	}
+}
+
+func (h *Harness) Shutdown(id ServerId) {
+	h.raftServers[id].Shutdown()
+	h.transports[id].Shutdown()
+	delete(h.raftServers, id)
+	// transport is retained for future reuse of serverID
+}
+
+// Restart restarts the server id, which must have been previously
+// shutdown with ShutdownServer.
+func (h *Harness) Restart(id ServerId) error {
+	if _, found := h.raftServers[id]; found {
+		return fmt.Errorf("Tried to restart server already running: %d", id)
+	}
+	var err error
+	newRaftServer, err := NewRaftServer(id, filter(h.serverIds, id), h.stateDirs[id])
+	newRaftServer.transport = h.transports[id] // reuse transport
+	if err != nil {
+		return fmt.Errorf("Error creating new server for leaderID %d", id)
+	}
+	h.raftServers[id] = newRaftServer
+	go h.transports[id].Serve()
+	go h.raftServers[id].Start()
+	return nil
+}
+
+func (h *Harness) Servers() iter.Seq[*RaftServer] {
+	return func(yield func(*RaftServer) bool) {
+		for _, rs := range h.raftServers {
+			if !yield(rs) {
+				return
+			}
+		}
+	}
+}
+func (h *Harness) PersistentStates(
+	t *testing.T,
+) iter.Seq2[ServerId, persistentState] {
+	return func(yield func(ServerId, persistentState) bool) {
+		for id, stateDir := range h.stateDirs {
+			ps, err := LoadPersistentState(stateDir)
+			if err != nil {
+				t.Fatalf("Failed to load persistent state for server %d: %v", id, err)
+			}
+			if !yield(id, ps) {
+				return
+			}
+		}
+	}
+}
+
+// filter removes items matching `remove` from `list`.
+// Used to create the list of peers for a given raft server such
+// that the list doesn't include the server itself.
+func filter[T comparable](list []T, remove T) []T {
+	filtered := []T{}
+	for _, item := range list {
+		if item != remove {
+			filtered = append(filtered, item)
+		}
+	}
+	return filtered
+}
