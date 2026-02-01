cd7f0f465e3ca8dce53d956054a5dca984e8dd84
Mike Rhodes <mike.rhodes@gmail.com>
2026-02-01 17:33:58 +0000

    feat: log replication - wait for lastApplied to catch up during client commands
    This commit adds logic to catchUp and processAppendEntriesRPC to
    move the lastApplied state forward to the latest commitIndex.
    
    It does not yet apply the log to the state machine. This will need to
    be an extra method on rpcOutgoingTransport or a new StateMachine
    interface. Probably the latter as the user of flotsam would need to
    provide it.
    
    Finally, the logic in processClientCommand is updated to wait on the
    lastApplied value catching up to the log entry the command added to the
    log, rather than waiting for a majority of servers to have added the
    entry to their log. This is correct per the paper, the previous
    behaviour was not, but was useful for testing the client command code
    previously.

--- raft/rpc_test.go
+++ raft/rpc_test.go
@@ -74,7 +74,7 @@ func Test_maybeUpdatedCommitIndex(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got := maybeUpdatedCommitIndex(
+			got := calculateUpdatedCommitIndex(
 				0, term, tt.currentCommitIndex, tt.matchIndex, tt.raftLog,
 			)
 			if got != tt.wantCommitIndex {
